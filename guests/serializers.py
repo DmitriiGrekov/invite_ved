0c6683d8c43c641369df0c049fc59a9412f2d13d
DGrekov <dgrekov@ibs.ru>
2025-04-27 13:24:30 +0500

    Удалил ненужные файлы

--- guests/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import GuestModel
-
-
-class GuestSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = GuestModel
-        fields = '__all__'
--- guests/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import GuestViewSet
-
-
-urlpatterns = [
-    path('', GuestViewSet.as_view())
-]
